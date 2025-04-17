11a659d303f86a4d8f000180ccf0991024480ce1
ferkanzai <fernando.carayu@gmail.com>
2025-04-17 14:23:46 +0200

    feat: add frontend Dockerfile
    Signed-off-by: ferkanzai <fernando.carayu@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,4 +21,10 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  }
 })
