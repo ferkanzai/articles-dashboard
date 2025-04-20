4649f3e65be30ce7cbf89671ccec05321e54dea4
ferkanzai <fernando.carayu@gmail.com>
2025-04-20 20:58:33 +0200

    fix: serve emoji favicon
    Signed-off-by: ferkanzai <fernando.carayu@gmail.com>

--- api/src/lib/create-app.ts
+++ api/src/lib/create-app.ts
@@ -2,7 +2,6 @@ import type { Hook } from "@hono/zod-openapi";
 import type { ErrorHandler, NotFoundHandler } from "hono";
 import type { ContentfulStatusCode, StatusCode } from "hono/utils/http-status";
 
-import { serveStatic } from "@hono/node-server/serve-static";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { requestId } from "hono/request-id";
 import { appendTrailingSlash } from "hono/trailing-slash";
@@ -13,6 +12,7 @@ import env from "@/env";
 import { INTERNAL_SERVER_ERROR, NOT_FOUND, OK, UNPROCESSABLE_ENTITY } from "@/lib/http-status-codes";
 import { connInfoMiddleware } from "@/middlewares/get-conn-info";
 import { logger } from "@/middlewares/logger";
+import serveEmojiFavicon from "@/middlewares/serve-emoji-favicon";
 
 const defaultHook: Hook<any, any, any, any> = (result, c) => {
   if (!result.success) {
@@ -56,13 +56,14 @@ export function createRouter() {
 
 export default function createApp() {
   const app = createRouter();
-  app.use("/api/favicon.ico", serveStatic({ path: "./favicon.ico" }));
-  app.use(appendTrailingSlash());
-  app.use(requestId());
-  app.use(logger());
-  app.use(connInfoMiddleware());
-  app.notFound(notFound);
-  app.onError(onError);
+  app
+    .use(serveEmojiFavicon("🚀"))
+    .use(appendTrailingSlash())
+    .use(requestId())
+    .use(logger())
+    .use(connInfoMiddleware())
+    .notFound(notFound)
+    .onError(onError);
 
   return app;
 }
