544fa7446cfded8e3db26f308d3f727173868547
ferkanzai <fernando.carayu@gmail.com>
2025-04-24 00:24:26 +0200

    fix: add suspense to filters
    Signed-off-by: ferkanzai <fernando.carayu@gmail.com>

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,9 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Suspense } from "react";
 import TanstackQueryLayout from "../integrations/tanstack-query/layout";
 import type { QueryClient } from "@tanstack/react-query";
+import { Spinner } from "@/components/spinner";
 import Filters from "@/components/filters";
 import ArticleHighlights from "@/components/articles/article-highlights";
 
@@ -15,7 +17,9 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       <div className="container mx-auto py-6 px-4 space-y-6 max-w-7xl">
         <h1 className="text-3xl font-bold">Article Dashboard</h1>
 
-        <Filters />
+        <Suspense fallback={<Spinner size="small" />}>
+          <Filters />
+        </Suspense>
         <div className="flex flex-col gap-4">
           <div>
             <ArticleHighlights />
